16af4fe593f7a20b796c068b7735f1efb7b831d4
MarconiJunior <junior@brumafe.com>
2023-01-01 17:19:29 -0300

    Dinamizando as informações

---
+++ assets/script/api.js
@@ -0,0 +1,8 @@
+
+
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/MarconiJunior/PortfolioPage/main/data/profile.json'
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
+}
